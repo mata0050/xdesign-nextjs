bce7b9a30d7bffd3e9785b84e358107a2d0c0b18
mata0050 <mata0050@outlook.com>
2022-05-25 20:56:28 -0400

    refactor

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,10 +1,7 @@
-
 import '../styles/tailwind.css';
 import Layout from '../components/Layout';
 
 function MyApp({ Component, pageProps }) {
-  console.log(pageProps)
-  
   return (
     <Layout>
       <Component {...pageProps} />
